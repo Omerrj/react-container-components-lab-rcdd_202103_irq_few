7ad5ea13bb4244e0b8fc575f02cc525c6d4fbbbf
Omer Sabah <omerrj99@gmail.com>
2021-06-16 23:47:54 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,33 +4,46 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const BASE_URL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  `api-key=${NYT_API_KEY}&query=`;
 
-class LatestMovieReviewsContainer extends Component {
-  constructor() {
-    super();
+class SearchableMovieReviewsContainer extends Component {
+  state = {
+    searchTerm: '',
+    reviews: []
+  };
 
-    this.state = {
-      reviews: []
-    };
-  }
+  handleSearchInputChange = event =>
+    this.setState({ searchTerm: event.target.value });
 
-  componentDidMount() {
-    fetch(URL)
+  handleSubmit = event => {
+    event.preventDefault();
+
+    fetch(BASE_URL.concat(this.state.searchTerm))
       .then(res => res.json())
-      .then(response => this.setState({ reviews: response.results }));
-  }
+      .then(res => this.setState({ reviews: res.results }));
+  };
 
   render() {
     return (
-      <div className="latest-movie-reviews">
-        <h2>The Latest Reviews:</h2>
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search-input">Search Movie Reviews</label>
+          <input
+            id="search-input"
+            type="text"
+            style={{ width: 300 }}
+            onChange={this.handleSearchInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {typeof this.state.reviews === 'object' &&
+          this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
         <MovieReviews reviews={this.state.reviews} />
       </div>
     );
   }
 }
 
-
-export default LatestMovieReviewsContainer;
+export default SearchableMovieReviewsContainer;
